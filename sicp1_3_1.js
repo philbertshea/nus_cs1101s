0b2125541b0fde3d4540e306715e035790254164
Source Academy User <No public email provided>
2023-08-19 23:52:08 +0800

    Updated with correct Ans

--- sicp1_3_1.js
+++ sicp1_3_1.js
@@ -1,3 +1,7 @@
+// Integral uses the Original method 
+// integral2 is my attempt at Simpson's Integral
+// integral3 is the most accurate because it avoids rounding errors from repetitive addition
+
 function sum(term, a, next, b) {
     return a>b ? 0 : term(a) + sum(term, next(a), next, b);
 }
@@ -6,7 +10,7 @@ function inc(x) {
     return x+1;
 }
 function is_even(x) {
-    return x % 2 === 0;
+    return x % 2 <0.1;
 }
 function cube(x) {
         return x * x * x;
@@ -30,16 +34,57 @@ function id(x) {
 
 function integral2(f, a, b, n) {
     const h = (b-a)/n;
-    const origA = a;
     function int_next(x) {
         return x + h;
     }
     function int_term(x) {
-        return ((x===origA || x===origA+n*h) ? 1 : is_even((x-origA)/h) ? 2 : 4) * f(x);
+        const iter = (x-a)/h;
+        const prefix = ((iter===0 || iter===n) 
+        ? 1 
+        : is_even((x-a)/h) 
+        ? 2 
+        : 4);
+        const y = prefix * f(x);
+        
+        display("Run : ");
+        display(iter);
+        display("Prefix : ");
+        display(prefix);
+        display("term(x): ");
+        display(y);
+        return y;
         
     }
-    return h/3*sum(int_term, a, int_next, a+n*h);
+    return h/3*sum(int_term, a, int_next, b);
+}
+
+function integral3(f, a, b, n) {
+    function helper(h) {
+        function y(k) { 
+            return f((k * h) + a);
+        }
+	function term(k) {
+            return k === 0 || k === n
+                   ? y(k)
+                   : k % 2 === 0
+                     ? 2 * y(k)
+                     : 4 * y(k);
+        }
+        return sum(term, 0, inc, n) * (h / 3);
+    }
+    return helper((b - a) / n);
+}
+
+
+function testRounding() {
+    const x = 0;
 }
 //sum_cubes(3, 5);
-integral(cube, 0, 1, 0.01);
-integral2(cube, 0, 1, 1000);
+//integral(cube, 0, 1, 0.01);
+integral2(cube, 0, 1, 10);
+//integral3(cube, 0, 1, 10);
+
+
+
+
+
