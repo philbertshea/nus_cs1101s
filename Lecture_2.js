9ba3ca856e2c3ded48456047b7d14c32bdd2470c
Source Academy User <No public email provided>
2023-08-22 20:00:58 +0800

    Preparation for Lec 2

--- Lecture_2.js
+++ Lecture_2.js
@@ -1,4 +1,4 @@
-import {heart, show, sail, rcross, nova, stack_frac} from "rune";
+import {heart, show, sail, rcross, nova, stack_frac, quarter_turn_right} from "rune";
 
 const heartsail = stack_frac(0.5, heart, sail);
 // returns a stacked image where 50% is the heart and 50% is the sail.
@@ -28,22 +28,64 @@ show(trisection);
 show(quadrisection);
 
 // My attempt: Linear, Recursive Process 
-function section(n, rune) {
-    return sect_iter(n, rune);
+function stackn(n, rune) {
+    return n===1 ? rune : stack_frac(1/n, rune, stackn(n-1, rune));
 }
 // From 1/4 --> 1/3 --> 1/2 --> heart
-function sect_iter(count, rune) {
-    return count===1 ? rune : stack_frac(1/count, rune, sect_iter(count-1, rune));
-}
+
+show(stackn(6, heart));
 
 // My attempt: Linear, Iterative Process
-function section_i(n, rune) {
-    function sect_iter_i(result, count, rune) {
-    return count>n ? result : sect_iter_i(stack_frac(1/count, rune, result), count+1, rune);
+function stackn_i(n, rune) {
+    function sect_iter_i(result, count) {
+    return count>n ? result : sect_iter_i(stack_frac(1/count, rune, result), count+1);
 }
-    return sect_iter_i(rune, 2, rune);
+    return sect_iter_i(rune, 2);
 }
 
-show(section_i(6, heart));
+show(stackn_i(6, sail));
 
-show(section(6, heart));
+
+// Define repeat_pattern
+/*
+repeat_pattern(3, make_cross, sail);
+            Should Lead to
+make_cross(make_cross(make_cross(sail)));
+*/
+
+
+// My attempt: Linear, Recursive Process
+function repeat_pattern(n, func, rune) {
+    // Call func n times on the rune
+    return n===0 ? rune : func(repeat_pattern(n-1, func, rune));
+}
+
+show(repeat_pattern(3, quarter_turn_right, heart));
+
+// My attempt: Linear, Iterative Process
+function repeat_pattern_i(n, func, rune) {
+    function repeat_iter_i(result, count) {
+        return count===0 ? result : repeat_iter_i(func(result), count-1);
+    }
+    return repeat_iter_i(rune, n);
+}
+
+show(repeat_pattern_i(2, quarter_turn_right, heart));
+
+
+// Define Fibonacci
+
+// My attempt: Linear, Recursive Process
+function fibonacci(n) {
+    return n===1 ? 1 : n * fibonacci(n-1);
+}
+// My attempt: Linear, Iterative Process
+function fibonacci_i(n) {
+    function fib_iter(result, count) {
+        return count===1 ? result : fib_iter(count*result, count-1);
+    }
+    return fib_iter(1, n);
+}
+
+display(fibonacci(4));
+display(fibonacci_i(5));
