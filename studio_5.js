32df901a1d4d44521d410b8a4802fef15290e4e9
Source Academy User <No public email provided>
2023-09-10 14:51:57 +0800

    Changes made from Source Academy

--- studio_5.js
+++ studio_5.js
@@ -16,6 +16,8 @@ display_all(c);
 
 // Q2
 
+display("-------------------------Q2----------------------");
+
 function reverse(lst) {
     return is_null(lst)
            ? null
@@ -27,11 +29,42 @@ display_all(d);
 
 // Q3
 
-const lst2 = list(list(7), list(6, 5, 4), list(3, 2), 1);
-const lst3 = list(7, list(6), list(5, list(4)), list(3, list(2, list(1))));
-const lst4 = list(7, list(list(6, 5), list(4), 3, 2), list(list(1)));
+display("-------------------------Q3----------------------");
+
+function head_disp(x) {
+    display_all(head(x));
+    return head(x);
+}
+
+function tail_disp(x) {
+    display_all(tail(x));
+    return tail(x);
+}
+
 
 const lst1 = list(7, list(6, 5, 4), 3, list(2, 1));
-const e = head(tail(head(tail(tail(tail(lst1))))));
+const e = head_disp(tail_disp
+                    (head_disp(tail_disp(tail_disp(tail_disp(lst1))))));
+display(e);
+display("-------------------------------------");
 
-display_all(e);
+const lst2 = list(list(7), list(6, 5, 4), list(3, 2), 1);
+const f = head_disp(tail_disp(tail_disp(tail_disp(lst2))));
+display(f);
+display("-------------------------------------");
+
+
+const lst3 = list(7, list(6), list(5, list(4)), list(3, list(2, list(1))));
+const g = head_disp(
+            head_disp(tail_disp(
+                head_disp(tail_disp(
+                    head_disp(tail_disp(tail_disp(tail_disp(lst3)))))))));
+display(g);
+display("-------------------------------------");
+
+
+const lst4 = list(7, list(list(6, 5), list(4), 3, 2), list(list(1)));
+const h = head_disp(
+            head_disp(
+                head_disp(tail_disp(tail_disp(lst4)))));
+display(h);
