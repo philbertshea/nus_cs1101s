cfdcafe0f6705127aba5009ad5e69f716d404590
Source Academy User <No public email provided>
2023-10-09 00:47:21 +0800

    CSE Clarifications

---
+++ CSE_1.js
@@ -0,0 +1,80 @@
+
+/* let x = 10;
+const c = 15;
+x = 20;
+let y = 30;
+x = y; */
+
+// 2 * 3 + 4 * 5;
+
+//1 + 5 / (2 - 1) * 2;
+
+//const x = 1;
+
+
+3*(5-1)/2;
+
+/*
+const x = true;
+const y = false;
+x ? true : y; 
+*/
+
+/*
+let x = 0;
+x = x + 1;
+*/
+
+/*
+let x = 1;
+
+if (x > 0) {
+    11;
+} else {
+    22;
+}*/
+
+/*
+let x=0;
+let y=0;
+let z=0;
+z = y = x = x + 1;*/
+
+/*
+let i = 2;
+while(i<1) {
+    i = i + 1;
+}*/
+/*
+const n = 5;
+let i = undefined;
+2;
+let k = undefined;
+let j = undefined;
+i = 1;
+let result = 1;
+for(i=1; i<=n; i=i+1) {
+    result = result * i;
+}*/
+/*
+const n = 5;
+2;
+let k = 0;
+4;
+const j = 1;
+5; 
+k = j + 1;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
