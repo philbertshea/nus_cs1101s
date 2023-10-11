7e4363c7cbf2d5b43435497405ffa0b479a080f0
Source Academy User <No public email provided>
2023-10-11 22:24:51 +0800

    Environment Model

---
+++ CSE_2.js
@@ -0,0 +1,117 @@
+
+/* let x = 10;
+const c = 15;
+x = 20;
+let y = 30;
+x = y; */
+
+// 2 * 3 + 4 * 5;
+
+//1 + 5 / (2 - 1) * 2;
+
+//const x = 1;
+
+
+// 3*(5-1)/2;
+
+/*
+const x = 1>0;
+const y = 3<0;
+x && y; 
+*/
+
+/*
+let x = 0;
+x = x + 1;
+*/
+
+/*
+let x = 1;
+
+if (x > 0) {
+    11;
+} else {
+    22;
+}*/
+
+/*
+let x=0;
+let y=0;
+let z=0;
+z = y = x = x + 1;*/
+
+/*
+let i = 2;
+while(i<1) {
+    i = i + 1;
+}*/
+/*
+const n = 5;
+let i = undefined;
+2;
+let k = undefined;
+let j = undefined;
+i = 1;
+let result = 1;
+for(i=1; i<=n; i=i+1) {
+    result = result * i;
+}*/
+/*
+const n = 5;
+2;
+let k = 0;
+4;
+const j = 1;
+5; 
+k = j + 1;*/
+
+/*
+const c = 1; 
+let x = c + 2;
+const d = c + x;
+let y = 3;
+x = c;
+y = d;*/
+
+/*
+1 + 2;
+const x = 1;
+3 * 4;
+let y = x - 7;
+*/
+
+/*
+const x = 1;
+{
+    const x = 6;
+}
+x + 1;
+*/
+
+
+/*const square = x => x * x;
+square(5);*/
+
+/*
+function cube(x) {
+const y = x * x * x;
+return y;
+y + 1;
+}
+cube(3);
+*/
+
+/*
+function make_adder(x) {
+    return y => x + y;
+}
+const add_4 = make_adder(4);
+add_4(5);
+*/
+
+
+
+
+
+
+
